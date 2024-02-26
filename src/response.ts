bfe78a02dcc91bbb46c574e382317e38d5097f62
t-iku <ixy.1998.dragon@gmail.com>
2024-02-26 21:21:08 +0900

    [add] NoriResponseクラスを実装
    htmlやjsonの扱いをもう少し楽にしたいという希望

---
+++ src/response.ts
@@ -0,0 +1,40 @@
+export class NoriResponse {
+  private _status: number;
+  private _headers = {};
+
+  constructor(status = 200) {
+    this._status = status;
+  }
+
+  public get status(): number {
+    return this.status;
+  }
+
+  public set staus(v: number) {
+    this._status = v;
+  }
+
+  public set headers(data: HeadersInit) {
+    this._headers = {
+      ...this._headers,
+      ...data,
+    };
+  }
+
+  public respond(data: BodyInit | null | undefined) {
+    return new Response(data, {
+      status: this._status,
+      headers: this._headers,
+    });
+  }
+
+  public json(data: object) {
+    this._headers = { "Content-Type": "application/json" };
+    return this.respond(JSON.stringify(data));
+  }
+
+  public html(data: string) {
+    this._headers = { "Content-Type": "text/html" };
+    return this.respond(data);
+  }
+}
